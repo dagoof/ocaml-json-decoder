463b41fd3f4c5c697ca5a909b0feb96e0dd6a4c4
Peter DeGroff <peter.degroff@ansarada.com>
2016-10-10 16:58:34 +1100

    fixed up tests

--- tests/test_json_decoder.ml
+++ tests/test_json_decoder.ml
@@ -1,23 +1,40 @@
 open Json_decoder
 
-let person () =
-    let open Decoder in
-    let decoder = "age" @= int in
-    let decoded = decode_string decoder "{\"age\": 28}" in
-    Alcotest.(check @@ result int string)
-        "person age"
-        ( Result.Ok 28 )
-        decoded
-
 let simple_decoders =
-    [ "Person", `Quick, person
-    ; "Person1", `Slow, person
-    ; "Person2", `Slow, person
-    ; "Person3", `Quick, person
+    let check checker got expected () =
+        Alcotest.(check @@ result checker string )
+        "primitive decoder"
+        got
+        expected
+    in
+    [ "int", `Quick,
+        check Alcotest.int
+        Decoder.(decode_string int "23")
+        ( Result.Ok 23 )
+    ; "int", `Quick,
+        check Alcotest.int
+        Decoder.(decode_string int "46")
+        ( Result.Ok 46 )
+    ; "float", `Quick,
+        check Alcotest.float
+        Decoder.(decode_string float "2.3")
+        ( Result.Ok 2.3 )
+    ; "string", `Quick,
+        check Alcotest.string
+        Decoder.(decode_string string "\"twenty-three\"")
+        ( Result.Ok "twenty-three" )
+    ; "bool-true", `Quick,
+        check Alcotest.bool
+        Decoder.(decode_string bool "true")
+        ( Result.Ok true )
+    ; "bool-false", `Quick,
+        check Alcotest.bool
+        Decoder.(decode_string bool "false")
+        ( Result.Ok false )
     ]
 
 
 let () =
     Alcotest.run "test suite"
-    [ "simple_decoders", simple_decoders
+    [ "primitives", simple_decoders
     ]

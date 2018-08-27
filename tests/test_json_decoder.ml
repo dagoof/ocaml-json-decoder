6de8f136afdd53144b0555384dcfd1fddb2b335d
Peter DeGroff <peter.degroff@gmail.com>
2018-08-27 23:14:34 +1000

    rebase me

--- tests/test_json_decoder.ml
+++ tests/test_json_decoder.ml
@@ -1,80 +1,75 @@
 open Json_decoder
 
-let check2 checker label got expected () =
-  Alcotest.(check @@ result checker string )
+let check_result checkable label got expected () =
+  Alcotest.(check @@ result checkable string)
     label
     got
     expected
 
-let check_result checker label got expected () =
-  Alcotest.(check @@ result checker string)
-    label
-    got
-    expected
-
-let tc
+let test_case
     ~label
     ?(speed=`Quick)
     ?(descr="")
-    checker
+    checkable
     got
     expected =
   Alcotest.(
     test_case
       label
       speed
-      (check_result checker descr got expected)
+      (check_result checkable descr got expected)
   )
 
-(*
-let succeed =
-    ~label
-    ~descr
-    checker
+let succeed
+    ?(speed=`Quick)
+    ?(descr="")
+    checkable
     decoder
-    content
+    source
     output =
-
-    tc
-      ~label ~descr
-      checker
-      Decoder.(decode_string decoder content)
-      output
-*)
-
+  test_case
+    ~speed
+    ~descr
+    checkable
+    Decoder.(decode_string decoder source)
+    (Result.Ok output)
 
 let primitives =
-  [ tc
+  [ test_case
       ~label:"int"
       ~descr:"decode a natural int"
       Alcotest.int
       Decoder.(decode_string int "23")
       (Result.Ok 23)
-  ; tc
+  ; succeed
+      ~label:"int wow"
+      Alcotest.int
+      Decoder.int "23" 23
+  ; test_case
       ~label:"int"
       ~descr:"decode a negative int"
       Alcotest.int
       Decoder.(decode_string int "-46")
       ( Result.Ok ~-46 )
-  ; tc
+  ; test_case
       ~label:"float"
       ~descr:"decode a float with some precision"
       Alcotest.(float 1.)
       Decoder.(decode_string float "2.3")
       ( Result.Ok 2.3 )
-  ; tc
+  ; test_case
       ~label:"string"
       ~descr:"decodes a plain string"
       Alcotest.string
       Decoder.(decode_string string "\"twenty-three\"")
       ( Result.Ok "twenty-three" )
-  ; tc
+  ; test_case
       ~label:"true"
       ~descr:"decodes a true bool"
       Alcotest.bool
       Decoder.(decode_string bool "true")
       ( Result.Ok true )
-  ; tc
+  ; test_case
       ~label:"false"
       ~descr:"decodes a false bool"
       Alcotest.bool
@@ -83,37 +78,37 @@ let primitives =
   ]
 
 let containers =
-  [ tc
+  [ test_case
       ~label:"list-int-index"
       Alcotest.int
       Decoder.(decode_string (index 1 int) "[1,48,3]")
       ( Result.Ok 48 )
-  ; tc
+  ; test_case
       ~label:"dict-field"
       Alcotest.(float 1.)
       Decoder.(decode_string (field "lat" float) "{\"lat\": 52.3}")
       ( Result.Ok 52.3 )
-  ; tc
+  ; test_case
       ~label:"dict-field"
       Alcotest.(float 1.)
       Decoder.(decode_string (field "lng" float) "{\"lat\": 52.3}")
       ( Result.Error "key lng does not exist in object lat " )
-  ; tc
+  ; test_case
       ~label:"list"
       Alcotest.(list int)
       Decoder.(decode_string (list int) "[1,48,3]")
       ( Result.Ok [1;48;3] )
-  ; tc
+  ; test_case
       ~label:"array"
       Alcotest.(array int)
       Decoder.(decode_string (array int) "[1,48,3]")
       ( Result.Ok [|1;48;3|] )
-  ; tc
+  ; test_case
       ~label:"pairs"
       Alcotest.(list (pair string int))
       Decoder.(decode_string (pairs int) "{\"lat\": 5, \"lng\": 15}")
       ( Result.Ok ["lat",5; "lng",15] )
-  ; tc
+  ; test_case
       ~label:"mapN"
       Alcotest.string
       Decoder.(decode_string (
@@ -128,14 +123,6 @@ let containers =
 
 
 
-(*
-  [ succeed
-      ~label:"int"
-      ~descr:"decode a plain int"
-      Alcotest.int
-      Decoder.int "23" 23
-  ]
-   *)
 let () =
     Alcotest.run "json_decoder"
     [ "primitives", primitives
